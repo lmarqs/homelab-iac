5a905eadb8169ff3b6a22e0b28862c01623f5aae
lmarqs <lucas.marques@medprevonline.com>
2025-01-22 19:36:39 -0300

    [main] delete unused modules

--- development/modules.tf
+++
@@ -1,8 +0,0 @@
-# module "dev-tools" {
-#   source = "./dev-tools"
-
-#   project = lxd_project.this.name
-#   images  = lxd_cached_image.jammy.fingerprint
-
-#   addresses = [2]
-# }
--- production/zabbix/versions.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_providers {
-    lxd = {
-      source = "terraform-lxd/lxd"
-    }
-  }
-}
