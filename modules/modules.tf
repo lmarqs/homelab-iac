bac71167ed300ab83b9e0c910886a4b448468934
Lucas Marques <lucas.marques@medprevonline.com>
2025-11-20 12:49:44 -0300

    remove unused "swe" module from modules.tf

--- modules/modules.tf
+++ modules/modules.tf
@@ -7,13 +7,3 @@ module "developer" {
     addresses = slice(var.macvlan.addresses, 0, 1)
   })
 }
-
-module "swe" {
-  source = "./swe"
-
-  image = lxd_cached_image.jammy.fingerprint
-
-  macvlan = merge(var.macvlan, {
-    addresses = slice(var.macvlan.addresses, 1, 2)
-  })
-}
